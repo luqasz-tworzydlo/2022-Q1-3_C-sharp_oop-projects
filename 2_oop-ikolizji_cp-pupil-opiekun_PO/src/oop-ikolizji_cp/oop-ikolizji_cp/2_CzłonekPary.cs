05b111b9e989b2447c18fe32d0aa57050c37645c
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-05-02 23:59:05 +0200

    Add files via upload

---
+++ 2_oop-ikolizji_cp-pupil-opiekun_PO/src/oop-ikolizji_cp/oop-ikolizji_cp/2_CzłonekPary.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    abstract class CzłonekPary // można użyć tej klasy instancjowach, ale może służyć jako typ referencji, zmiennej [ale nie może użyć jako typ instancji]
+    {
+        protected CzłonekPary para; // pole
+        // protected pozwala na dostęp klasom dziedziczącym
+        // private nie pozwala na dostęp
+        virtual public CzłonekPary Para // właściwość
+        {
+            get { return para; }
+            set
+            {
+                if (para != null)
+                    para.para = null;
+                para = value;
+                if (para != null)
+                    para.para = this;
+            }
+        }
+        /*abstract public string Identyfikacja();*/ // nasza metoda
+        abstract public string Identyfikacja // nasza właściwość => może to być potrzebne, jeśli oczekujemy, aby ta własność miała dostęp do klasy, która nie jest zdefiniowana
+        {
+            get;
+        }
+    }
+    class Pupil : CzłonekPary
+    {
+        public override CzłonekPary Para
+        {
+            get => base.Para;
+            set
+            {
+                if (value is Opiekun)
+                    base.Para = value;
+                else
+                    throw new Exception(
+                        "Nieprawidłowy typ - Parą dla Pupil powinien być Opiekun"
+                        );
+            }
+            // { get => base.Para; set => base.Para = value; } // autouzupełnianie
+        }
+        public Opiekun Opiekun
+        {
+            get { return (Opiekun)Para; }
+            set { Para = value; }
+        }
+        public string
+            Imię,
+            Gatunek,
+            Rasa;
+        // poniżej zdefyniowana identyfikacja
+        public override string Identyfikacja
+        {
+            get { return $"{Gatunek} {Rasa} {Imię}"; }
+        }
+        public override string ToString()
+        {
+            return Imię;
+        }
+    }
+    // virtual musi mieć zaimplementowaną metodą, zaś abstract deleguje implementację na klasy dziedziczące
+    class Opiekun : CzłonekPary
+    {
+        public override CzłonekPary Para
+        {
+            get => base.Para;
+            set
+            {
+                if (value is Pupil)
+                    base.Para = value;
+                else
+                    throw new Exception(
+                        "Nieprawidłowy typ - Parą dla Opiekun powinien być Pupil"
+                        );
+            }
+        }
+        public Pupil Pupil
+        {
+            get { return (Pupil)Para; }
+            set { Para = value; }
+        }
+        public string
+            Imię,
+            Nazwisko;
+        public override string Identyfikacja
+        {
+            get { return $"{Imię} {Nazwisko}"; }
+        }
+        public override string ToString()
+        {
+            return Imię;
+        }
+    }
+}
---
+++ 2_oop-ikolizji_cp-pupil-opiekun_PO/src/oop-ikolizji_cp/oop-ikolizji_cp/4_Figura.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    abstract class Figura
+    {
+        double
+            środek_X,
+            środek_Y;
+
+        double Pole { get; }
+        double Obwód { get; }
+        double Średnica { get; }
+        abstract public string Właściwości_Figury
+        {
+            get;
+        }
+
+    }
+    class ProstokątF : Figura
+    {
+        public double
+            BokA,
+            BokB;
+
+        // poniżej jest konstruktor
+        public ProstokątF(
+            double bokA,
+            double bokB)
+        {
+            BokA = bokA;
+            BokB = bokB;
+        }
+
+        public double Średnica
+        {
+            get => Math.Sqrt(
+                BokA * BokA + BokB * BokB
+                );
+        }
+        public double Obwód
+        {
+            get { return 2 * BokA + 2 * BokB; }
+        }
+        public double Pole
+        {
+            get { return BokA * BokB; }
+        }
+        public override string Właściwości_Figury
+        {
+            get { return $"=> Pole prostokątu wynosi: {Pole} \n=> Obwód prostokątu wynosi: {Obwód} \n=> Średnica prostokątu wynosi: {Średnica}"; }
+        }
+    }
+    class KołoF : Figura
+    {
+        public double Promień;
+        public (double x, double y) Środek { get; set; }
+
+        // poniżej jest konstruktor
+        public KołoF(
+            double promień,
+            (double x, double y) środek
+            )
+        {
+            Promień = promień;
+            Środek = środek;
+        }
+
+        public double Średnica
+        {
+            get => 2 * Promień;
+        }
+        const double PI = 3.14;
+        public double Obwód
+        {
+            get { return 2 * PI * Promień * Promień; }
+        }
+        public double Pole
+        {
+            get { return PI * Promień * Promień; }
+        }
+
+        public override string Właściwości_Figury
+        {
+            get { return $"=> Pole koła wynosi: {Pole} \n=> Obwód koła wynosi: {Obwód} \n=> Średnica koła wynosi: {Średnica}"; }
+        }
+        /*public override string ToString()
+        {
+            return $"=> Pole koła wynosi: {Pole} \n=> Obwód koła wynosi: {Obwód} \n=> Średnica koła wynosi: {Średnica}";
+        }*/
+    }
+    class TrójkątF_równoboczny : Figura
+    {
+        public double
+            BokA;
+
+        // poniżej jest konstruktor
+        public TrójkątF_równoboczny(
+            double bokA)
+        {
+            BokA = bokA;
+        }
+
+        public double Średnica
+        {
+            get { return (BokA * Math.Sqrt(3) / 2); }
+        }
+        public double Pole
+        {
+            get { return (BokA * Math.Sqrt(3) / 4); }
+        }
+        public double Obwód
+        {
+            get { return BokA + BokA + BokA; }
+        }
+        public override string Właściwości_Figury
+        {
+            get { return $"=> Pole trójkąta równobocznego wynosi: {Pole} \n=> Obwód trójkąta równobocznego wynosi: {Obwód} \n=> Średnica [wysokość] trójkąta równobocznego wynosi: {Średnica}"; }
+        }
+    }
+    class TrójkątF_prostokątny : Figura
+    {
+        public double
+            BokA,
+            BokB,
+            BokC;
+
+        // poniżej jest konstruktor
+        public TrójkątF_prostokątny(
+            double bokA,
+            double bokB,
+            double bokC)
+        {
+            BokA = bokA;
+            BokB = bokB;
+            BokC = bokC;
+        }
+
+        public double Średnica
+        {
+            get { return BokC; }
+        }
+        public double Pole
+        {
+            get { return (BokA * BokB / 2); }
+        }
+        public double Obwód
+        {
+            get { return BokA + BokB + BokC; }
+        }
+        public override string Właściwości_Figury
+        {
+            get { return $"=> Pole trójkąta prostokątnego wynosi: {Pole} \n=> Obwód trójkąta prostokątnego wynosi: {Obwód} \n=> Średnica [wysokość] trójkąta prostokątnego wynosi: {Średnica}"; }
+        }
+    }
+}
